bf5b7d1336c7cb54d0fc4a4df0b61edbcb84bc09
rtds8 <64678924+rtds8@users.noreply.github.com>
2020-09-16 18:36:42 +0530

    ritwik_rtds8's chess game
    The game is still in the developing phase.

---
+++ Assets/My_Chess_Game/Scripts/Selection_Handler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selection_Handler : MonoBehaviour
+{
+    [SerializeField] private Camera m_camera;
+    private Transform m_current, m_previous;
+
+    private void OnValidate()
+    {
+        if (m_camera == null)
+            m_camera = Camera.main;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 mouse_position = Input.mousePosition;
+        Ray ray = m_camera.ScreenPointToRay(mouse_position);
+
+        Debug.DrawRay(ray.origin, ray.direction * 10f, Color.green);
+
+        if(Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            m_current = raycastHit.transform;
+            if (m_previous == null)
+            {
+                m_previous = m_current;
+                m_current.position += new Vector3(0, 0.1f, 0);
+            }
+            
+            if (m_current != m_previous)
+            {
+                m_previous.position -= new Vector3(0, 0.1f, 0);
+                m_current.position += new Vector3(0, 0.1f, 0);
+                m_previous = m_current;
+            }
+        }
+        else
+        {
+            m_previous.position -= new Vector3(0, 0.1f, 0);
+            m_previous = null;
+        }
+    }
+}
