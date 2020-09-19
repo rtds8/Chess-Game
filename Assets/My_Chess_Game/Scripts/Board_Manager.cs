90b25cecf97ac77498de435e509bd0042d604e53
rtds8 <64678924+rtds8@users.noreply.github.com>
2020-09-19 16:13:47 +0530

    ritwik_rtds8's_Chess_Game

---
+++ Assets/My_Chess_Game/Scripts/Board_Manager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board_Manager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
